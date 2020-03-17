4814301fa5eded63913851a7004044879599c6c4
ThomasFeldman <tfeldman1.31@gmail.com>
2020-03-17 13:25:58 -0500

    Updating state

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@
     constructor(props) {
         super(props);
 
+        //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+        //TO THIS.STATE
         this.state = { lat: null };
+    
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //we called setstate!!!!
+                this.setState({ lat: position.coords.latitude });
+            
+                //we did not!!!! NEVER DO THIS
+                //this.state.lat = position.coords.latitude
+            },
+            (err) => console.log(err)
+         );
     }
     
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-         );
-
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
